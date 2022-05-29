939689ca46b8fff12c1254f8dd381bbcfe7b74fb
tinkamara <85729793+tinkamara@users.noreply.github.com>
2022-05-29 21:01:46 +0200

    First UnitTests

--- JUINT/PlayerTest.java
+++ JUINT/PlayerTest.java
@@ -1,7 +1,42 @@
 import org.junit.jupiter.api.*;
 
 public class PlayerTest {
+    static Player tester1;
+     Player tester2;
+  String message;
+
+    Exception ex = null;
+
     @BeforeAll
+    static void setupPlayer1() {
+        try {
+            tester1 = Player.createPlayer("Tester1");
+        } catch (NoMorePlayersAllowedException e) {
+        }
+    }
+    @Test
+    void getName(){
+        String name = tester1.getName();
+        Assertions.assertEquals(name, "Tester1");
+    }
+    @Test
+    void getToken(){
+        char token = tester1.getToken();
+        Assertions.assertEquals(token, 'X');
+    }
+    @Test
+    public void create() throws NoMorePlayersAllowedException {
+        tester2 = Player.createPlayer("Tester2");
+        Assertions.assertTrue(tester2 != null);
 
-
+    }
+    @Test
+    public void noMorePlayersAllowedException(){
+        try {
+            Player tester3 = Player.createPlayer("Tester3");
+        } catch (NoMorePlayersAllowedException e) {
+            ex = e;
+        }
+        Assertions.assertTrue(ex instanceof NoMorePlayersAllowedException);
+    }
 }
